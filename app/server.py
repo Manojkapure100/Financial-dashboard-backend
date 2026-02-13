e8cde583e9299bd22af39f17491ea33eb226204f
manoj@ubuntu <manojkapure100@gmail.com>
2026-02-13 21:49:37 +0000

    fixed origin

--- app/server.py
+++ app/server.py
@@ -2,30 +2,34 @@
 Main application entry point
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.v1 import routes_market, routes_capital_market
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.dbModels import Stock
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.logger import logger
 
 app = FastAPI(title="Financial Dashboard Backend")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4200",
-        "http://localhost:4200/",
-        "https://manojkapure100.github.io",
-        "https://manojkapure100.github.io/",
-        "https://manojkapure100.github.io/Financial-dashboard",
-        "https://manojkapure100.github.io/Financial-dashboard/"
+        "http://localhost:4200",       # local dev
+        "https://manojkapure100.github.io",  # GitHub Pages
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.middleware("http")
+async def log_origin(request, call_next):
+    print("Origin header:", request.headers.get("origin"))
+    logger.info(f"Origin header: {request.headers.get("origin")}")
+    response = await call_next(request)
+    return response
 
 # Include routers
 app.include_router(routes_market.router)
